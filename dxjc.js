705fa2ac86b4fd486029552f1809ee034b4ad291
CunYu <fff.linyi@gmail.com>
2016-12-20 10:51:49 +0800

    添加判断条件, 添加README.

--- dxjc.js
+++ dxjc.js
@@ -10,7 +10,7 @@ if (document.title == ""){ // 检查标题是否为空, 被劫持标题一般都
   if (html.split("<iframe").length-1 != -1){ // 寻找iframe, 被劫持一定会有iframe的.
     var url = document.URL;
     var src = document.getElementById("contenid").src;
-    if (src == url+"?"){ // 检查iframe的地址, 确认是否被劫持.
+    if (src == url+"?" || src == url+"&"){ // 检查iframe的地址, 确认是否被劫持.
       // 刷新页面.
       top.location.href = url;
       // 保存被运营商劫持的次数.
