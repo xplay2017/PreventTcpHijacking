9113297604bea3d57cd1e88f0fe61b6cc35243ce
CunYu <fff.linyi@gmail.com>
2017-02-12 00:22:33 +0800

    修复访问无标题网页读取不到contenid报错的问题.

--- dxjc.js
+++ dxjc.js
@@ -9,28 +9,31 @@ if (document.title == ""){ // 检查标题是否为空, 被劫持标题一般都
   var html = document.body.outerHTML;
   if (html.split("<iframe").length-1 != -1){ // 寻找iframe, 被劫持一定会有iframe的.
     var url = document.URL;
-    var src = document.getElementById("contenid").src;
-    if (src == url+"?" || src == url+"&"){ // 检查iframe的地址, 确认是否被劫持.
-      // 获取广告URL
-      var u = html.split("var content='")[1];
-      // 请求广告URL, 让劫持者认为已点击广告.
-      if (u){
-        u = u.split("'")[0];
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", u,true);
-        xhr.send(null);
-      }
-      // 刷新页面.
-      top.location.href = url;
-      // 保存被运营商劫持的次数.
-      if (window.localStorage){
-        var num = localStorage['num'];
-        if (!parseInt(num)){
-          num = "0";
+    var contenid = document.getElementById("contenid");
+    if (contenid){
+      var src = contenid.src;
+      if (src == url+"?" || src == url+"&"){ // 检查iframe的地址, 确认是否被劫持.
+        // 获取广告URL
+        var u = html.split("var content='")[1];
+        // 请求广告URL, 让劫持者认为已点击广告.
+        if (u){
+          u = u.split("'")[0];
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", u,true);
+          xhr.send(null);
         }
-        num = parseInt(num) + 1;
-        chrome.storage.sync.set({'num': String(num)});
-        localStorage['num'] = num;
+        // 刷新页面.
+        top.location.href = url;
+        // 保存被运营商劫持的次数.
+        if (window.localStorage){
+          var num = localStorage['num'];
+          if (!parseInt(num)){
+            num = "0";
+          }
+          num = parseInt(num) + 1;
+          chrome.storage.sync.set({'num': String(num)});
+          localStorage['num'] = num;
+        }
       }
     }
   }
