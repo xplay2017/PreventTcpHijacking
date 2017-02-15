6f2d876b62139f7a0fd1176186d59d9999891910
CunYu <fff.linyi@gmail.com>
2017-02-16 01:31:17 +0800

    优化"模拟点击广告"代码, 添加"模拟点击广告"开关.

--- dxjc.js
+++ dxjc.js
@@ -1,7 +1,16 @@
-chrome.storage.sync.get("num",function(date){
-  if (date['num'] == "-1"){
-    localStorage['num'] = "0";
-    chrome.storage.sync.set({'num': "0"});
+chrome.storage.sync.get("num",function(data){
+  if (data['num'] == -1){
+    localStorage['num'] = 0;
+    chrome.storage.sync.set({'num': 0});
+  }
+})
+
+chrome.storage.sync.get("clickAD",function(data){
+  if(data["clickAD"]){
+    localStorage['clickAD'] = data["clickAD"];
+  }else{
+    localStorage['clickAD'] = false;
+    chrome.storage.sync.set({'clickAD': false});
   }
 })
 
@@ -13,28 +22,75 @@ if (document.title == ""){ // 检查标题是否为空, 被劫持标题一般都
     if (contenid){
       var src = contenid.src;
       if (src == url+"?" || src == url+"&"){ // 检查iframe的地址, 确认是否被劫持.
-        // 获取广告URL
-        var u = html.split("var content='")[1];
-        // 请求广告URL, 让劫持者认为已点击广告.
-        if (u){
-          u = u.split("'")[0];
-          var xhr = new XMLHttpRequest();
-          xhr.open("GET", u,true);
-          xhr.send(null);
+        // 判断是否需要模拟点击广告
+        if (localStorage['clickAD']){
+          // 获取广告URL
+          var u = html.split("var content='")[1];
+          // 请求广告URL, 让劫持者认为已点击广告.
+          if (u){
+            u = u.split("'")[0];
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", u, true);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState == 4 && xhr.status == 200) {
+                var dom = loadXMLString(xhr.responseText);
+                var theForm = dom.forms['form1'];
+                if (!theForm) {
+                  theForm = dom.form1;
+                }
+                if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+                  var xhr1 = new XMLHttpRequest();
+                  xhr1.open("POST", u, true);
+                  xhr1.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                  var data = "__EVENTTARGET=btnNoNeed&__EVENTARGUMENT=&__VIEWSTATE=";
+                  data += encodeURIComponent(theForm.__VIEWSTATE.value);
+                  data += "&__VIEWSTATEGENERATOR=";
+                  data += theForm.__VIEWSTATEGENERATOR.value;
+                  data += "&__EVENTVALIDATION=";
+                  data += encodeURIComponent(theForm.__EVENTVALIDATION.value);
+                  xhr1.send(data);
+                }
+              }
+            };
+            xhr.send(null);
+          }
         }
         // 刷新页面.
         top.location.href = url;
         // 保存被运营商劫持的次数.
         if (window.localStorage){
           var num = localStorage['num'];
-          if (!parseInt(num)){
-            num = "0";
+          if (!num){
+            num = 0;
           }
-          num = parseInt(num) + 1;
-          chrome.storage.sync.set({'num': String(num)});
+          num += 1;
+          chrome.storage.sync.set({'num': num});
           localStorage['num'] = num;
         }
       }
     }
   }
+}
+
+/*字符串转dom对象*/
+function loadXMLString(txt)
+{
+  try //Internet Explorer
+   {
+     xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+     xmlDoc.async="false";
+     xmlDoc.loadXML(txt);
+     return(xmlDoc);
+   }
+  catch(e)
+   {
+     try //Firefox, Mozilla, Opera, etc.
+      {
+        parser=new DOMParser();
+        xmlDoc=parser.parseFromString(txt,"text/xml");
+        return(xmlDoc);
+      }
+     catch(e) {alert(e.message)}
+   }
+  return(null);
 }
--- localStorage.js
+++ localStorage.js
@@ -1,13 +1,24 @@
 if (window.localStorage){
-  chrome.storage.sync.get("num",function(date){
-    var num = date['num'];
+  chrome.storage.sync.get("num",function(data){
+    var num = document.getElementById("num");
     if (parseInt(num) > 0){
-      document.getElementById("num").innerHTML = "你被嘿嘿嘿了" + num + "次.";
+      num.innerHTML = "你被嘿嘿嘿了" + data['num'] + "次.";
+    }else{
+      num.innerHTML = "你的浏览器还是纯洁的.";
     }
-  })
+  });
+  chrome.storage.sync.get("clickAD",function(data){
+    if (data['clickAD']){
+      document.getElementById("clickAD").checked = true;
+    }
+  });
 }
 
 document.getElementById("rs").addEventListener('click', function(){
-  chrome.storage.sync.set({'num': "-1"});
-  document.getElementById("num").innerHTML = "你还是纯洁的.";
+  chrome.storage.sync.set({'num': -1});
+  document.getElementById("num").innerHTML = "你的浏览器还是纯洁的.";
+}, false);
+
+document.getElementById("clickAD").addEventListener('click', function(){
+  chrome.storage.sync.set({'clickAD': document.getElementById("clickAD").checked});
 }, false);
